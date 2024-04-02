caf0a4fa81323745655127550d70bc607f2d2c07
mkheck13 <mkheck13@gmail.com>
2024-04-02 08:36:14 -0700

    readme updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,111 @@
-import Image from "next/image";
+'use client'
+import logoComponent from "./components/logoComponent";
+import dollarIconComponent from "./components/dollarIconComponent";
+import personIconComponent from "./components/personIconComponent";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [billVal, setBillVal] = useState<number>(0)
+  const [billInputVal, setBillInputVal] = useState<string>('');
+  const [tipVal, setTipVal] = useState<number>(0)
+  const [pplVal, setPplVal] = useState<number>(0)
+  const [pplInputVal, setPplInputVal] = useState<string>('');
+  const [tipAmount, setTipAmount] = useState<number>(0)
+  const [totalAmount, setTotalAmount] = useState<number>(0)
+  let value: number;
+
+  useEffect(() => {
+    if (billVal !== 0 && pplVal !== 0 && tipVal !== 0) {
+      let billTip = billVal * (tipVal / 100);
+      setTipAmount(billTip / pplVal);
+      setTotalAmount((billVal + billTip) / pplVal)
+    }
+    else { setTipAmount(0), setTotalAmount(0) }
+
+  }, [billVal, tipVal, pplVal])
+
+  const handleReset = () => {
+    setBillVal(0)
+    setPplVal(0)
+    setTipVal(0)
+    setBillInputVal('');
+    setPplInputVal('');
+  }
+
+  useEffect(()=>{
+    pplInputVal && Number.parseInt(pplInputVal) <= 0 ? (document.querySelector('#pplSpan')?.classList.remove('invisible'), document.querySelector('#PplInput')?.classList.add('focus:border-red-500')) : (document.querySelector('#pplSpan')?.classList.add('invisible'), document.querySelector('#PplInput')?.classList.remove('focus:border-red-500'))   
+  },[pplInputVal])
+
+  const buttonFunc = (label: string, value: number) => {
+    return (
+      <button
+        className="h-14 w-36 rounded-lg text-white bg-VDcyan text-2xl focus:bg-Scyan hover:bg-LGcyan hover:text-VDcyan focus:text-VDcyan"
+        onClick={() => setTipVal(value)}>
+        {label}
+      </button>
+    )
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-LGcyan flex min-h-screen flex-col items-center md:p-24">
+      <div className="w-24 py-12 md:py-24">
+        {logoComponent()}
+      </div>
+      <div className="md:w-7/8 h-fit min-h-[500px] flex flex-wrap md:flex-nowrap  justify-center bg-white rounded-3xl">
+        <div className="col ml-12 mr-8 my-8 md:w-1/2 flex flex-col justify-around">
+
+          <label className="block">
+            <span className="block text-md font-medium text-DGcyan">
+              Bill
+            </span>
+            <span className="absolute flex items-center my-5 mx-2">
+              {dollarIconComponent()}
+            </span>
+            <input id="BillInput" type="Bill" inputMode="numeric" name="Bill" className="text-end text-VDcyan text-2xl mt-1 px-3 py-2 bg-VLGcyan border-2 border-transparent shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" value={billInputVal} onChange={(e) => {setBillVal(Number.parseInt(e.target.value)), setBillInputVal(e.target.value)}} />
+          </label>
+
+          <div className="block">
+            <span className="block text-md font-medium text-DGcyan">
+              Select Tip %
+            </span>
+            <div className="flex flex-wrap justify-between gap-y-4">
+              {buttonFunc('5%', 5)}
+              {buttonFunc('10%', 10)}
+              {buttonFunc('15%', 15)}
+              {buttonFunc('25%', 25)}
+              {buttonFunc('50%', 50)}
+              <input className="h-14 w-36 rounded-lg text-DGcyan bg-VLGcyan text-2xl focus:ring-0 text-center" placeholder="Custom" onChange={(e) => setTipVal(Number.parseInt(e.target.value))}></input>
+            </div>
+          </div>
+
+          <label className="block">
+            <span className="flex flex-row justify-between text-md font-medium text-DGcyan">
+              Number of People <span id="pplSpan" className="invisible text-red-400 peer-invalid:visible">Can't be Zero</span>
+            </span>
+            <span className="absolute flex items-center my-5 mx-2">
+              {personIconComponent()}
+            </span>
+            <input id="PplInput" type="People" name="People" className="text-end  text-VDcyan text-2xl border-2 border-transparent mt-1 px-3 py-2 bg-VLGcyan shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" min={1} aria-required="true" value={pplInputVal} onChange={(e) => {setPplVal(Number.parseInt(e.target.value)), setPplInputVal(e.target.value)}} />
+          </label>
+
+        </div>
+        <div className="flex flex-col justify-between col w-full h-72 md:w-1/2 mr-8 md:mr-12 ml-8 my-8 p-8 bg-VDcyan rounded-3xl">
+          <div className="row h-2/5 flex flex-col justify-between gap-y-4">
+            <div className="row flex flex-row justify-between">
+              <p className="text-white md:text-lg">Tip Amount<br /><span className="text-DGcyan text-md">/ person</span></p>
+              <p className="text-Scyan text-4xl md:text-5xl">{'$' + Math.round(tipAmount * 100) / 100}</p>
+            </div>
+            <div className="row flex flex-row justify-between">
+              <p className="text-white md:text-lg">Total<br /><span className="text-DGcyan text-md">/ person</span></p>
+              <p className="text-Scyan text-4xl md:text-5xl">{'$' + Math.round(totalAmount * 100) / 100}</p>
+            </div>
+          </div>
+          <div className="row">
+            <button className="w-full h-12 bg-Scyan rounded-lg text-3xl text-DGcyan" onClick={() => { handleReset() }}>RESET</button>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
